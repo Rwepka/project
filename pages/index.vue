874a2b880594cbafea0a5aae2d26c7acf4ad8778
Rwepka <Rwepka@gmail.com>
2023-04-23 16:08:47 +0300

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,5 @@
+<template>
+    <section>
+        <div class="head">Это домашняя страница</div>
+    </section>
+</template>
---
+++ pages/notahome.vue
@@ -0,0 +1,5 @@
+<template>
+    <section>
+        <div class="Head">Это не домашняя страница</div>
+    </section>
+</template>
